b9f2ccecac4a4419ea1b8fa1ec9fe22e18a88bc7
kimizuy <kimizuyamasaki@gmail.com>
2024-07-05 00:40:20 -0700

    make simple

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,62 +1,40 @@
 import { ipcRenderer, contextBridge } from "electron";
 
-const allowedChannels = ["main-process-message"] as const;
+const allowedChannels = ["main-process-message", "perform-heavy-task"] as const;
 
-type AllowedChannel = (typeof allowedChannels)[number];
+export type AllowedChannel = (typeof allowedChannels)[number];
 
-const electronApi = {
+const api = {
   on(
     channel: AllowedChannel,
     listener: Parameters<typeof ipcRenderer.on>[1],
   ): void {
-    if (allowedChannels.includes(channel)) {
-      ipcRenderer.on(channel, listener);
-    } else {
-      console.warn(`Channel ${channel} is not allowed`);
-    }
+    ipcRenderer.on(channel, listener);
   },
   off(
     channel: AllowedChannel,
     listener: Parameters<typeof ipcRenderer.off>[1],
   ): void {
-    if (allowedChannels.includes(channel)) {
-      ipcRenderer.off(channel, listener);
-    } else {
-      console.warn(`Channel ${channel} is not allowed`);
-    }
+    ipcRenderer.off(channel, listener);
   },
   send(
     channel: AllowedChannel,
     ...args: Parameters<typeof ipcRenderer.send>[1]
   ): void {
-    if (allowedChannels.includes(channel)) {
-      ipcRenderer.send(channel, ...args);
-    } else {
-      console.warn(`Channel ${channel} is not allowed`);
-    }
+    ipcRenderer.send(channel, ...args);
   },
   async invoke(
     channel: AllowedChannel,
     ...args: Parameters<typeof ipcRenderer.invoke>[1]
   ): ReturnType<typeof ipcRenderer.invoke> {
-    if (allowedChannels.includes(channel)) {
-      try {
-        return await ipcRenderer.invoke(channel, ...args);
-      } catch (error) {
-        console.error(`Error invoking channel ${channel}:`, error);
-        throw error;
-      }
-    } else {
-      console.warn(`Channel ${channel} is not allowed`);
-      return Promise.reject(`Channel ${channel} is not allowed`);
-    }
+    return await ipcRenderer.invoke(channel, ...args);
   },
 };
 
-contextBridge.exposeInMainWorld("electron", electronApi);
+contextBridge.exposeInMainWorld("electron", api);
 
 declare global {
   interface Window {
-    electron: typeof electronApi;
+    electron: typeof api;
   }
 }
