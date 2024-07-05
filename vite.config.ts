658e572fc4aac51617a33ef00662cd15e7e74042
kimizuy <kimizuyamasaki@gmail.com>
2024-07-05 09:59:27 -0700

    add path alias & use defineConfig()

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,29 @@
+import path from "path";
+import { defineConfig } from "vite";
 import electron from "vite-plugin-electron/simple";
 
-export default {
+const resolve = {
+  alias: {
+    "@": path.resolve(__dirname, "./src"),
+  },
+};
+
+export default defineConfig({
   plugins: [
     electron({
       main: {
         entry: "electron/main.ts",
+        vite: {
+          resolve,
+        },
       },
       preload: {
         input: "electron/preload.ts",
+        vite: {
+          resolve,
+        },
       },
     }),
   ],
-};
+  resolve,
+});
