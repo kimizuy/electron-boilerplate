9686c7adf3e98be9dd053e54d45065934ca15d3c
kimizuy <kimizuyamasaki@gmail.com>
2024-09-25 15:07:55 -0700

    use invoke method only & improve type

--- src/preload.ts
+++ src/preload.ts
@@ -3,32 +3,22 @@ import { type ipcMainListeners } from "./ipc-main-listeners";
 
 type AllowedChannel = keyof typeof ipcMainListeners;
 
-// Publish a part of ipcRenderer API to the renderer process
-// ref: https://github.com/electron-vite/electron-vite-react/blob/f3e8e2cf6892b5d15eb974d1c3a8218f5f0d501b/electron/preload/index.ts#L3
+type InvokeArgs<K extends AllowedChannel> = Parameters<
+  (typeof ipcMainListeners)[K]
+>[1];
+
+type IsOptional<T> = undefined extends T ? true : false;
+
 const api = {
-  invoke<K extends AllowedChannel>(
+  invoke: <K extends AllowedChannel>(
     channel: K,
-    ...args: Parameters<(typeof ipcMainListeners)[K]>[1][]
-  ): Promise<Awaited<ReturnType<(typeof ipcMainListeners)[K]>>> {
+    ...args: IsOptional<InvokeArgs<K>> extends true
+      ? [InvokeArgs<K>?]
+      : InvokeArgs<K> extends undefined
+        ? []
+        : [InvokeArgs<K>]
+  ): Promise<Awaited<ReturnType<(typeof ipcMainListeners)[K]>>> => {
     return ipcRenderer.invoke(channel, ...args);
-  },
-  send<K extends AllowedChannel>(
-    channel: AllowedChannel,
-    ...args: Parameters<(typeof ipcMainListeners)[K]>[1][]
-  ): ReturnType<typeof ipcRenderer.send> {
-    return ipcRenderer.send(channel, ...args);
-  },
-  on(
-    channel: AllowedChannel,
-    listener: Parameters<typeof ipcRenderer.on>[1],
-  ): ReturnType<typeof ipcRenderer.on> {
-    return ipcRenderer.on(channel, listener);
-  },
-  off(
-    channel: AllowedChannel,
-    listener: Parameters<typeof ipcRenderer.off>[1],
-  ): ReturnType<typeof ipcRenderer.off> {
-    return ipcRenderer.off(channel, listener);
   },
 };
 
