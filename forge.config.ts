75a6c0d13ae7c9eada4de507a947e152ae467491
kimizuy <kimizuyamasaki@gmail.com>
2025-06-23 17:21:30 +0900

    feat: update config to build win app

--- forge.config.ts
+++ forge.config.ts
@@ -1,5 +1,4 @@
 import type { ForgeConfig } from "@electron-forge/shared-types";
-import { MakerSquirrel } from "@electron-forge/maker-squirrel";
 import { MakerZIP } from "@electron-forge/maker-zip";
 import { MakerDeb } from "@electron-forge/maker-deb";
 import { MakerRpm } from "@electron-forge/maker-rpm";
@@ -14,7 +13,7 @@ const config: ForgeConfig = {
   },
   rebuildConfig: {},
   makers: [
-    new MakerSquirrel({}),
+    new MakerZIP({}, ["win32"]),
     new MakerZIP({}, ["darwin"]),
     new MakerRpm({}),
     new MakerDeb({}),
